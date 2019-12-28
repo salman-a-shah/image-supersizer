5b66cd04f6af377eb4dc869fc957bfe71453672b
Salman Shah <salmanashah92@gmail.com>
2019-12-27 17:52:43 -0800

    updated flask app to new page

--- app.py
+++ app.py
@@ -17,7 +17,27 @@ model = core_functions.Predictor()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template("index.html")
+	imagefilename = ""
+	
+	if request.method == 'POST':
+	    # check if the post request has the file part
+	    if 'file' not in request.files:
+	        flash('No file part')
+	        return redirect(request.url)
+	    file = request.files['file']
+	    # if user does not select file, browser also
+	    # submit an empty part without filename
+	    if file.filename == '': # Todo: bug. not working
+	        flash('No selected file')
+	        return redirect(request.url)
+	    if file and allowed_file(file.filename):
+	        filename = secure_filename(file.filename)
+	        imagefilename = filename
+	        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	        return redirect(url_for('prediction',
+	                                filename=filename))
+	        
+	return render_template("index.html", imagefilename=imagefilename)
 
 @app.route('/start', methods=['GET', 'POST'])
 def upload_file():
@@ -42,10 +62,10 @@ def upload_file():
 	                                filename=filename))
 	return render_template("upload.html", imagefilename=imagefilename)
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'],
+#                                filename)
 
 @app.route('/prediction/<filename>')
 def prediction(filename):
