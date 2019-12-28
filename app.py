c065ce263a9cca12da98308a2c65790d2f86f132
Salman Shah <salmanashah92@gmail.com>
2019-12-27 18:15:04 -0800

    Cleaned up main app file

--- app.py
+++ app.py
@@ -39,38 +39,10 @@ def index():
 	        
 	return render_template("index.html", imagefilename=imagefilename)
 
-@app.route('/start', methods=['GET', 'POST'])
-def upload_file():
-	imagefilename = ""
-	
-	if request.method == 'POST':
-	    # check if the post request has the file part
-	    if 'file' not in request.files:
-	        flash('No file part')
-	        return redirect(request.url)
-	    file = request.files['file']
-	    # if user does not select file, browser also
-	    # submit an empty part without filename
-	    if file.filename == '': # Todo: bug. not working
-	        flash('No selected file')
-	        return redirect(request.url)
-	    if file and allowed_file(file.filename):
-	        filename = secure_filename(file.filename)
-	        imagefilename = filename
-	        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-	        return redirect(url_for('prediction',
-	                                filename=filename))
-	return render_template("upload.html", imagefilename=imagefilename)
-
-# @app.route('/uploads/<filename>')
-# def uploaded_file(filename):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'],
-#                                filename)
-
 @app.route('/prediction/<filename>')
 def prediction(filename):
 	model.predict(filename)
 	return send_from_directory(PREDICITON_FOLDER, filename)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
